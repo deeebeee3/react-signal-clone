9ad34f1dd315e403df6fbb99f26281331e2161a9
Deepak Bhari <deepak.bhari@lifts.to>
2021-01-31 17:36:28 +0000

    Basic Login Screen styled

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import { KeyboardAvoidingView } from "react-native";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
   return (
-    <View>
-      <Text>I am the login screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{
+          width: 200,
+          height: 200,
+        }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //take up entire height
+    alignItems: "center", //horizontally center elements
+    justifyContent: "center", //vertically center elements
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});

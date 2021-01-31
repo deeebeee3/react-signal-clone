c7a8bebe84029247a5221c1ca077a244438b8db9
Deepak Bhari <deepak.bhari@lifts.to>
2021-01-31 22:43:46 +0000

    Show registered user avatar on home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,39 @@
-import React from "react";
-import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { Touchable } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { Avatar } from "react-native-elements";
 import CustomListItem from "../components/CustomListItem";
+import { auth, db } from "../firebase";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    console.log(auth.currentUser.photoURL);
+    navigation.setOptions({
+      title: "Signal",
+      headerStyle: { backgroundColor: "#fff" },
+      headerTitleStyle: { color: "#000" },
+      headerTintColor: "#000",
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <TouchableOpacity>
+            <Avatar
+              rounded
+              source={{
+                uri: auth?.currentUser?.photoURL,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, []);
+
   return (
     <SafeAreaView>
       <ScrollView>
